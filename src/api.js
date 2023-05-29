d2f126fe9bd5f3d1f74b24918972b5ecb69c13a5
George-Florin Petrila <floringpetrila@gmail.com>
2023-05-29 17:28:27 +0300

    First commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e50315c74bmsh5e8687bfa58bd39p10df7ajsn7ac4ed735fbf",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "eec30cb0980cffb055b2643a1e82b241";
